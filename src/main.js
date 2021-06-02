0a3fbb321ad54581e3650fdfba61f7400e212bb1
xiaodeng <927561237@qq.com>
2021-06-02 10:48:36 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
+/*
+   项目的启动入口
+*/
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 导入 element 组件库
+import ElementUl from 'element-ui'
+
+// 导入element组件库样式文件
+import 'element-ui/lib/theme-chalk/index.css'
+
 // 加载全局样式文件
 import './styles/index.less'
+
+// 全局注册element组件库 安装插件
+Vue.use(ElementUl)
 
 Vue.config.productionTip = false
 
